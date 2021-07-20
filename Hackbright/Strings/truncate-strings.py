e7081e2f9f3ec034d4a7ddaeb7b40b1eb947afb0
Hannah Ramadan <hannahr315@gmail.com>
2021-07-20 03:04:19 +0000

    updated truncate string problem

--- Hackbright/Strings/truncate-strings.py
+++ Hackbright/Strings/truncate-strings.py
@@ -1,12 +1,13 @@
 def truncate(string):
     """Truncate repeating characters in a string."""
 
-    current = 0
     new_s = ""
     
-    for i in range(len(string)):
-        if string[i] != current:
-            new_s = new_s + string[i]
-        current = string[i]
+    if string:
+        current = 0
+        for i in range(len(string)):
+            if string[i] != current:
+                new_s = new_s + string[i]
+            current = string[i]
     
     return new_s
